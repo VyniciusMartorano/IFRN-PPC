6cce04297062296a282465cfee8f309457a7e0be
VYNICIUS MARTORANO <VyniciusMartorano>
2022-06-03 22:48:52 -0300

    questao01

--- atividades/Lista03/01.py
+++ atividades/Lista03/01.py
@@ -1,2 +1,18 @@
+"""
+Fazer um programa que solicite ao usuário um valor inteiro positivo e gere o seu valor em binário.
+ATENÇÃO: Deverá ser utilizada divisões inteiras para a solução.
+"""
 
+num = int(input('Digite um valor para seu binario: '))
 
+resta = 0
+binario = ''
+while True:
+    resta = num % 2
+    quo = num // 2
+    binario += str(resta)
+    num = quo
+    if quo < 1:
+        break
+
+print(binario[::-1])
