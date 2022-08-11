0918ab31598a0f5d06197cca36b3f2667e1af413
VYNICIUS MARTORANO <VyniciusMartorano>
2022-08-11 20:36:46 -0300

    update

--- Lista07/Questão-01.py
+++ Lista07/Questão-01.py
@@ -17,41 +17,35 @@ for root, sp, arqs in os.walk(path):
         ext = os.path.splitext(arq)[1]
         if ext == '.csv':
             with open(arq, 'r', encoding='utf-8') as file:
-                cont = 0
+                file.readline()
                 while True:
                     try:
                         line = file.readline()[:-1]
-                        if cont == 0:
-                            ...
-                        elif cont == 2:
-                            break
-                        else:
-                            txt_splited = line.split(';')
-                            full_text.append(
-                                [txt_splited[0], txt_splited[1], txt_splited[10],
-                                txt_splited[11], txt_splited[12], txt_splited[14]]
-                                )
                         if line == '':
                             break
+                        txt_splited = line.split(';')
+                        full_text.append(
+                            [txt_splited[0], txt_splited[1], txt_splited[10],
+                            txt_splited[11], txt_splited[12], txt_splited[14]]
+                            )
                     except:
                         ...
-                    cont += 1
                 file.close()
-            try:
-                with open(f'{path}\dados_estatisticos\serie_historica_anp.txt', 'w+') as file:
-                    for linha in full_text:
-                        value = ''
-                        for item in linha:
-                            value += item + ';'
-                        value += '\n'
-                        file.write(value)
-                    file.close()
-            except FileExistsError:
-                ...
+
+
+try:
+    with open(f'{path}\dados_estatisticos\serie_historica_anp.txt', 'w+') as file:
+        for linha in full_text:
+            value = ''
+            for item in linha:
+                value += item + ';'
+            value += '\n'
+            file.write(value)
+        file.close()
+except FileExistsError:
+    ...
                 
 
 #posições
 # 0, 1, 10, 11, 12, 14
 
-for i in full_text:
-    print(f'\n{i}\n')
